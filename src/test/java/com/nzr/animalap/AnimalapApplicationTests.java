fdef18fbd5c03085eea62938f04442932fc29890
reloadearth <yy2947257303@163.com>
2023-03-13 10:04:05 +0800

    去除了误上传的文件

---
+++ src/test/java/com/nzr/animalap/AnimalapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nzr.animalap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
